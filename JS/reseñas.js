9d71de9ca58e57c54dfae3460dd9b53c2fdfdf82
CamiEst <camilolorah@gmail.com>
2023-11-17 20:49:28 -0500

    Se crea la posible versión final del footer

--- JS/reseñas.js
+++ JS/reseñas.js
@@ -12,16 +12,10 @@ function showReview(direction){
     const reviews = document.querySelectorAll('.review__item');
     const totalReviews = reviews.length;
 
-    console.log(`Total de reseñas: ${totalReviews}`);
-
     review = (review + direction + totalReviews) % totalReviews;
-    console.log(`Mostrando reseña ${review + 1} de ${totalReviews}`);
 
     reviews.forEach((rev, index) => {
         const move = review * -100;
-
-        console.log(`review ${index + 1} - desplazamiento: ${move}%`)
         rev.style.transform = `translateX(${move}%)`;
     });
-    console.log(reviews.length);
 }
